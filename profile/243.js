62eff654e83450972b21daebbd258d36ef95335b
Maxim Kolesnikov <mxklsn@gmail.com>
2023-10-19 19:52:33 +0700

    gh-pages

---
+++ profile/243.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkprofile=self.webpackChunkprofile||[]).push([[243],{243:(e,l,n)=>{n.r(l),n.d(l,{default:()=>a});var r=n(900),t=n.n(r);const a=()=>t().createElement("div",{style:{margin:"10px",padding:"10px",textAlign:"center",backgroundColor:"green"}},t().createElement("h1",null,"Profile"))}}]);
